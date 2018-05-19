4e7fea4bd50db588de66b0626591ae0ab09cc5fd
freddy-mrqz <freddymrqz28@gmail.com>
2018-05-18 23:35:52 -0500

    moved bar-card example to Bar.js

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,15 +1,34 @@
 import React from 'react';
 
 const bar = (props) => (
-		<div class='card'>
-			<h2>{name}</h2>
-			<div id="card-content">
-				<div className='info'>
-					<p>{address}</p>
-				</div>
-			</div>
+	<span id='entire'>
+	<section className='section'>
+	<div className='container'>
+			<h1 className="title has-text-left">Lakeview</h1>
+			<hr/>
+			 <div className="is-divider" />
+				<a><div id='barBox' class='box'>
+					<article class='media'>
+						<div class='media-left'>
+							<figure class="image is-64x64">
+								<img src="https://bulma.io/images/placeholders/128x128.png" alt="Image"/>
+							</figure>
+						</div>
+						<div class='media-content'>
+							<div class='content'>
+							<p>
+								<strong><a>Bar Name</a></strong><br></br>
+								Address<br></br>
+								Happy hour times
+							</p>
+							</div>
+						</div>
+					</article>
+				</div></a>
 		</div>
-);
+	</section> 
+	</span> 
+)
 
 export default bar;
 	
--- src/components/Bars.js
+++ src/components/Bars.js
@@ -14,36 +14,8 @@ class Bars extends Component {
             <Bar key={index} {...bar}/>
         ))}
       </div>
-    );
-			<span id='entire'>
-      <section className='section'>
-			<div className='container'>
-					<h1 className="title has-text-left">Lakeview</h1>
-					<hr/>
-					 <div className="is-divider" />
-						<a><div id='barBox' class='box'>
-							<article class='media'>
-								<div class='media-left'>
-									<figure class="image is-64x64">
-        						<img src="https://bulma.io/images/placeholders/128x128.png" alt="Image"/>
-      						</figure>
-								</div>
-								<div class='media-content'>
-									<div class='content'>
-									<p>
-										<strong><a>Bar Name</a></strong><br></br>
-										Address<br></br>
-										Happy hour times
-									</p>
-									</div>
-								</div>
-							</article>
-						</div></a>
-				</div>
-			</section> 
-			</span> 
     )
-  }
+  }  
 }
 
 export default Bars;
