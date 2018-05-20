783f55701a4b6a91be87c17a8e8ff69abefb97b1
freddy-mrqz <freddymrqz28@gmail.com>
2018-05-20 16:33:46 -0500

    google maps api

--- client/src/components/Neighborhoods.js
+++ client/src/components/Neighborhoods.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 import '../App.css'
-//import Bars from './Bars.js'
+//import Bars from './Bars.js']
+
+const MyMapComponent =  withScriptjs(withGoogleMap((props) =>
+		<GoogleMap
+			defaultZoom={11}
+			defaultCenter={{ lat: 41.8781, lng: -87.6298 }}
+		>
+			{props.isMarkerShown && <Marker position={{ lat: 41.8781, lng: -87.6298 }} />}
+		</GoogleMap>
+))
+ 
 class Neighborhoods extends Component {
 
 	constructor(props) {
@@ -23,6 +34,10 @@ class Neighborhoods extends Component {
 		console.log('Form value: ' + this.state.inputvalue);
 	}
 
+	componentDidMount() {
+	
+  } 
+
 	render () {
 		return (
 			<div id="field">
@@ -32,6 +47,13 @@ class Neighborhoods extends Component {
 						<input className="button is-primary" type="submit" value="Search" />
 					</form>
 				</div>
+				<MyMapComponent 
+					isMarkerShown 
+					googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=AIzaSyAyt8XEpBmVkz9BjKc1-My-q7OoxTyvt8c"
+					loadingElement={<div style={{ height: `100%` }} />}
+					containerElement={<div style={{ height: `400px` }} />}
+					mapElement={<div style={{ height: `100%` }} />}
+				/>
 			</div>
 		);
 	}
