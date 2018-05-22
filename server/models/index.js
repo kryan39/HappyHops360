7bdd6c47da884da645a0ef07797e4e05ae6accaa
freddy-mrqz <freddymrqz28@gmail.com>
2018-05-22 11:01:05 -0500

    index.js typo

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const mongoose - require("mongoose");
+const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/happyhops", {
