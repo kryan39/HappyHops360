cb0abfedfe92d849e9bb45c8a2d3e41331266359
freddy-mrqz <freddymrqz28@gmail.com>
2018-05-22 11:17:34 -0500

    authenticating users

---
+++ server/handlers/auth.js
@@ -0,0 +1,14 @@
+const db = require("../models");
+const jwt = require("jsonwebtoken")
+
+exports.signup = async function(req, res, next){
+  try {
+
+  } catch(err) {
+    
+  }
+};
+
+exports.signin = function(){
+
+};
