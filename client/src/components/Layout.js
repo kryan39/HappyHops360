09f2e6241435260de144dc2d2e86a3fdfd28807e
Katie <katie@loop-depaulsecure-252-216.depaulsecure-student.depaul.edu>
2018-05-22 17:56:46 -0500

    slight changes to login and landing, sign up page work in progress

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
-import Map from './Mapsearch'
+import Map from './Mapsearch';
+import Bars from './Bars';
 class Layout extends Component {
+
+
 
   render() {
     return(
       <div id="layout">
       <Navbar />
       <Map/>
+      <Bars/>
     </div>
     )
   }
