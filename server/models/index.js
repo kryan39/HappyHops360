283065cb1fbd73cbc0ae5edaa0fee17d7c47c3e2
freddy-mrqz <freddymrqz28@gmail.com>
2018-05-22 11:50:18 -0500

    user registration and authentication

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,6 @@ mongoose.set("debug", true);
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/happyhops", {
   keepAlive: true,
-  useMongoClient: true
 });
+
+module.exports.User = require("./user");
--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
-    trype: String,
+    type: String,
     required: true,
     unique: true
   },
