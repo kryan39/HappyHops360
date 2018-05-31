244beda8c17b7de7d5588b45351f89119a3d6f66
freddy-mrqz <freddymrqz28@gmail.com>
2018-05-31 14:56:01 -0500

    bar turned into stateful component; modal setup

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Popup from 'reactjs-popup';
+import Modal from 'react-responsive-modal';
 
-const bar = (props) => (
-	
+class Bar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			open: false
+		}
+	}
+
+	onOpenModal = () => {
+    this.setState({modalLoading: "is-loading",
+    open: true
+    });
+  }
+
+  onCloseModal = () => {
+    this.setState({open: false});
+  }
+
+	render() {
+		return (
 			<div id='barBox' className='box'>
-				<article className='media'>
-					<div className='media-content'>
-						<div className='content'>
-							<p>
-								<strong>{ props.name }</strong><br/>
-								{props.formatted_address}<br/>
-							</p>
-								{props.rating} stars
-						</div>
+				<Modal id="modal" open={this.state.open} onClose={this.onCloseModal} classNames={{modal: 'custom-modal' }}>
+					<div style={{float: "left"}}>
+						<h2 style={{textAlign: "center"}}>Name: {this.props.name}</h2>
 					</div>
-				</article>
+					<br/>
+					<div style={{float: "left"}}>
+						<h2 style={{textAlign: "center"}}>Price Level: {this.props.price_level}</h2>
+					</div>
+					<br/>
+					<div style={{float: "left"}}>
+						<h2 style={{textAlign: "center"}}>Website:</h2>
+					</div>
+					<br/>
+					<div style={{float: "left"}}>
+						<h2 style={{textAlign: "center"}}>Location:</h2>
+					</div>
+					<br/>
+					<div style={{float: "left"}}>
+						<h2 style={{textAlign: "center"}}>Reviews:</h2>
+					</div>
+					<br/>
+        </Modal>
+				<button id="barbuttons" onClick={()=> this.onOpenModal()}>
+					<article className='media'>
+						<div className='media-content'>
+							<div className='content'>
+								<p>
+									<strong>{ this.props.name }</strong>
+								</p>
+								<p>
+									{this.props.formatted_address}
+								</p>
+								{this.props.rating} stars
+							</div>
+						</div>
+					</article>
+			 	</button>
 			</div>
+		)
+	}
+}
 
-)
-
-export default bar;
+export default Bar;
 	
-
